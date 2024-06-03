5a9b75c7f3a14c2c14e1f50d8785ae58cd990aa9
Fenrir89 <Forkabam99@gmail.com>
2024-06-03 16:52:08 +0300

    Add files via upload

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -34,16 +34,17 @@ if [ "$PUID" != 0 ] && [ "$PGID" != 0 ]; then
 	groupmod -o -g "$PGID" prosody
 fi
 
-if ! test -d /snikket/prosody; then
+#if ! test -d /snikket/prosody; then
 	install -o prosody -g prosody -m 750 -d /snikket/prosody;
-fi
+#fi
 
 chown -R prosody:prosody /var/spool/anacron /var/run/prosody /snikket/prosody /etc/prosody
 
 ## Generate secret for coturn auth if necessary
-if ! test -f /snikket/prosody/turn-auth-secret-v2; then
+#if ! test -f /snikket/prosody/turn-auth-secret-v2; then
+	rm -rf /snikket/prosody/turn-auth-secret-v2
 	head -c 32 /dev/urandom | base64 > /snikket/prosody/turn-auth-secret-v2;
-fi
+#fi
 
 # COMPAT w/ alpha.20200513: remove older format
 if test -f /snikket/prosody/turn-auth-secret; then
